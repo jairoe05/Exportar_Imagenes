77784f52993632e29a2f1b78b89404aa4834b557
Jairo Esquivel <a.diego.dc@gmail.com>
2017-04-26 10:55:58 -0600

    add Proyecto completo Funcional Basico
    Exporta las imagenes de una base de datos sql server, y guarda los
    archivos en una carpeta en local

---
+++ Save_Pictures/Save_Pictures/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Save_Pictures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPicture = new System.Windows.Forms.Button();
+            this.pgBarCarga = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnPicture
+            // 
+            this.btnPicture.Location = new System.Drawing.Point(30, 62);
+            this.btnPicture.Name = "btnPicture";
+            this.btnPicture.Size = new System.Drawing.Size(136, 23);
+            this.btnPicture.TabIndex = 0;
+            this.btnPicture.Text = "Exportar Imagenes";
+            this.btnPicture.UseVisualStyleBackColor = true;
+            this.btnPicture.Click += new System.EventHandler(this.btnPicture_Click);
+            // 
+            // pgBarCarga
+            // 
+            this.pgBarCarga.Location = new System.Drawing.Point(12, 110);
+            this.pgBarCarga.Name = "pgBarCarga";
+            this.pgBarCarga.Size = new System.Drawing.Size(186, 13);
+            this.pgBarCarga.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 133);
+            this.Controls.Add(this.pgBarCarga);
+            this.Controls.Add(this.btnPicture);
+            this.Name = "Form1";
+            this.Text = "Guardar Imagenes en LocalHost";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPicture;
+        private System.Windows.Forms.ProgressBar pgBarCarga;
+    }
+}
+
---
+++ Save_Pictures/Save_Pictures/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace Save_Pictures
+{
+    public partial class Form1 : Form
+    {
+        public static object Using { get; private set; }
+        #region Events
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPicture_Click(object sender, EventArgs e)
+        {
+            optenerRuta();
+        }
+        #endregion
+
+        #region guardado de imagenes metodos
+        // ------------ Optiene la ruta donde van a ser guardadas las imagenes
+        public void optenerRuta()
+        {
+            //guardar solo un archivo
+            //SaveFileDialog sfd = new SaveFileDialog();
+            //sfd.InitialDirectory = @"D:\Pictures\";
+            //sfd.RestoreDirectory = true;
+            //sfd.FileName = "*.jpg";
+            //sfd.DefaultExt = ".jpg";
+            //sfd.Filter = "jpg files (*.jpg)|*.jpg";
+
+            //if (sfd.ShowDialog() == DialogResult.OK)
+            //{
+            //   SaveProcess(sfd.OpenFile().ToString());
+            //}
+            // selecciona una carpeta
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Seleccione la ruta";
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                SaveProcess(fbd.SelectedPath.ToString()+"\\");
+            }
+        }
+        //-----------------combierte los bytes almacenados en la base de datos y los guarda formato jpg
+        private static void WriteBinaryFile(string fileName, byte[] data)
+        {
+            if ((string.IsNullOrEmpty(fileName)))
+                throw new ArgumentException("No se ha especificado el archivo de destino.", "fileName");
+
+            if ((data == null))
+                throw new ArgumentException("Los datos no son válidos para crear un archivo.", "data");
+
+            // Crear el archivo. Se producirá una excepción si ya existe
+            // un archivo con el mismo nombre.
+            using (FileStream fs = new FileStream(fileName, FileMode.CreateNew, FileAccess.Write))
+            {
+                // Crea el escritor para la secuencia.
+                BinaryWriter bw = new BinaryWriter(fs);
+
+                // Escribir los datos en la secuencia.
+                bw.Write(data);
+            }
+        }
+
+        // proceso de guardado de las imagenes
+        public void SaveProcess(string urlSave)
+        {
+            byte[] data = null;
+            try
+            {
+                // Construimos los correspondientes objetos para
+                // conectarnos a la base de datos de SQL Server,
+                // utilizando la seguridad integrada de Windows NT.
+
+                using (SqlConnection cnn = new SqlConnection(@"Data Source =GMSQL\TRAVERSE11; initial catalog = TES; user id= sa; password = Osi-149016!; MultipleActiveResultSets = True;"))
+                {
+                    // Creamos un comando.
+                    SqlCommand cmd = cnn.CreateCommand();
+
+                    // Seleccionamos únicamente el campo que contiene
+                    // los documentos, filtrándolo mediante su
+                    // correspondiente campo identificador.
+
+                    cmd.CommandText = "SELECT ISNULL(P.PictItem,'') FOTO, ISNULL(P.PictId,'') ID FROM tblInItemPict P WITH(NOLOCK)";// where PictId = \'001004 \'";
+
+                    // Abrimos la conexión.
+                    cnn.Open();
+
+                    // Creamos un DataReader.
+                    SqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                    // Leemos el registro.
+                    int bufferSize, total_filas = 0;
+                    pgBarCarga.Minimum = 0;
+                    while (dr.Read())
+                    {
+                        pgBarCarga.Increment(1);
+                        total_filas++;
+                        // El tamaño del búfer debe ser el adecuado para poder
+                        // escribir en el archivo todos los datos leídos.
+                        //
+                        // Si el parámetro 'buffer' lo pasamos como Null, obtendremos
+                        // la longitud del campo en bytes.
+
+                        bufferSize = Convert.ToInt32(dr.GetBytes(0, 0, null, 0, 0));
+
+                        // Creamos el array de bytes. Como el índice está
+                        // basado en cero, la longitud del array será la
+                        // longitud del campo menos una unidad.
+                        //
+                        data = new byte[bufferSize];
+
+                        // Leemos el campo.
+                        //
+                        dr.GetBytes(0, 0, data, 0, bufferSize);
+
+                        // Procedemos a crear el archivo, en el ejemplo
+                        // un documento de Microsoft Word.
+                        WriteBinaryFile(urlSave + dr["ID"].ToString() + ".png", data);
+                        // WriteBinaryFile("D:\\Pictures\\Imagenes_Productos\\" + dr["ID"].ToString() + ".jpg", data);
+                    }
+                    // Cerramos el objeto DataReader, e implícitamente la conexión.
+                    dr.Close();
+                    pgBarCarga.Maximum = total_filas;
+                }
+                pgBarCarga.Value = pgBarCarga.Maximum;
+                MessageBox.Show("Archivos exportados correctamente.", "Guardado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+    }
+}
